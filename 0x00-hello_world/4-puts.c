4b9f3184974fd12c14024e4cf5f7d1f6f96c3739
SfisoGentz <sfisogentz02.ramahlo@gmail.com>
2023-03-06 13:56:19 +0100

    Update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with put function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My first pointers_arrays_string readme
